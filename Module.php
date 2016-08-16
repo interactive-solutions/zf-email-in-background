56f919bcd8415680906c8f8faf0db39e15be9f82
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2016-08-16 09:35:30 +0200

    Implemented the module

--- Module.php
+++ Module.php
@@ -9,10 +9,25 @@ declare(strict_types = 1);
 
 namespace InteractiveSolutions\EmailInBackground;
 
+use Zend\Loader\StandardAutoloader;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
 class Module implements ConfigProviderInterface, AutoloaderProviderInterface
 {
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 
+    public function getAutoloaderConfig()
+    {
+        return array(
+            StandardAutoloader::class => [
+                StandardAutoloader::LOAD_NS => [
+                    __NAMESPACE__ => __DIR__ . '/src/',
+                ],
+            ],
+        );
+    }
 }
